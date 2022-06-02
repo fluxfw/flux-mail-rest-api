d8d4d81074e55b47457c9939a875878b451df69d
fluxfw <fluxfw@fluxlabs.ch>
2022-06-02 15:50:21 +0200

    routes ui

--- src/Adapter/Route/FetchRoute.php
+++ src/Adapter/Route/FetchRoute.php
@@ -3,12 +3,17 @@
 namespace FluxMailRestApi\Adapter\Route;
 
 use FluxMailRestApi\Libs\FluxMailApi\Adapter\Api\MailApi;
+use FluxMailRestApi\Libs\FluxMailApi\Adapter\Mail\MailDto;
 use FluxMailRestApi\Libs\FluxRestApi\Adapter\Body\JsonBodyDto;
+use FluxMailRestApi\Libs\FluxRestApi\Adapter\Body\Type\DefaultBodyType;
 use FluxMailRestApi\Libs\FluxRestApi\Adapter\Method\DefaultMethod;
 use FluxMailRestApi\Libs\FluxRestApi\Adapter\Method\Method;
+use FluxMailRestApi\Libs\FluxRestApi\Adapter\Route\Documentation\RouteDocumentationDto;
+use FluxMailRestApi\Libs\FluxRestApi\Adapter\Route\Documentation\RouteResponseDocumentationDto;
 use FluxMailRestApi\Libs\FluxRestApi\Adapter\Route\Route;
 use FluxMailRestApi\Libs\FluxRestApi\Adapter\Server\ServerRequestDto;
 use FluxMailRestApi\Libs\FluxRestApi\Adapter\Server\ServerResponseDto;
+use FluxMailRestApi\Libs\FluxRestApi\Adapter\Status\DefaultStatus;
 
 class FetchRoute implements Route
 {
@@ -29,15 +34,31 @@ class FetchRoute implements Route
     }
 
 
-    public function getDocuRequestBodyTypes() : ?array
+    public function getDocumentation() : ?RouteDocumentationDto
     {
-        return null;
-    }
-
-
-    public function getDocuRequestQueryParams() : ?array
-    {
-        return null;
+        return RouteDocumentationDto::new(
+            $this->getRoute(),
+            $this->getMethod(),
+            "Fetch emails",
+            null,
+            null,
+            null,
+            null,
+            [
+                RouteResponseDocumentationDto::new(
+                    DefaultBodyType::JSON,
+                    null,
+                    MailDto::class . "[]",
+                    "Mails"
+                ),
+                RouteResponseDocumentationDto::new(
+                    DefaultBodyType::TEXT,
+                    DefaultStatus::_400,
+                    null,
+                    "No json body"
+                )
+            ]
+        );
     }
 
 
